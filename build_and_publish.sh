412504a78ce4ba7b7986e96ea70db348fc20e6ce
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-11-09 23:59:07 +0900

    日本語表記を削除。

--- build_and_publish.sh
+++ build_and_publish.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
-# ja) GitHubPagesにしている簡易MavenRepositryにバイナリをアップするスクリプト。
-# en) upload scripts to MavenRepositry as a GitHubPages
+# upload scripts to MavenRepositry as a GitHubPages
 
-# ja) 先ずはmasterに移動
-# en) move to master
+#  move to master
 git checkout master
 
-# ja) まずは、バイナリー一式へとビルド。
-# en) 1st build all.
+# 1st build all.
 sbt clean publish
 
-# ja) Github-Pagesのブランチへ移動。1
-# en) move to Github-Pages branch
+# move to Github-Pages branch
 git checkout gh-pages
 
-# ja) 設定により、./target/repo にビルドした一式が入っているはずなので、そこからトップへコピー。
-# en) built files will be in './target/repo', copy them into the top
+# built files will be in './target/repo', copy them into the top
 cp -rf ./target/repo/com ./
 
 # git commit
@@ -23,6 +18,5 @@ git add ./
 git commit ./ -m 'Build and publish Binally.'
 git push
 
-# ja) ブランチを元に戻す。
-# en) roll back branch
+# roll back branch
 git checkout master
